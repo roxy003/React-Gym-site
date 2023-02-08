f42ff71e2fabb25690bd0f1e1cabd72189857f0c
roxy003 <monalshah03@gmail.com>
2023-02-08 23:27:23 +0530

    making commits

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,14 @@ import Logo from "../../assets/logo.png";
 const Header = () => {
   return (
     <div className="header">
-      <img src={Logo} alt=" " />
+      <img src={Logo} alt=" " className="logo" />
+      <ul className="header-menu">
+        <li>Home</li>
+        <li>Programs</li>
+        <li>Why us</li>
+        <li>Plans</li>
+        <li>Testimonials</li>
+      </ul>
     </div>
   );
 };

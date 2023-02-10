3138e9312971036b75a0f7c4898fbe999887fd00
roxy003 <monalshah03@gmail.com>
2023-02-11 02:24:00 +0530

    making commits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
-import Programs from "./components/Programs/Programs"; //Needed to import this file compulsory it seems so
+import Programs from "./components/Programs/Programs";
+import Reasons from "./components/Reasons/Reasons";
+//Needed to import this file compulsory it seems so
 function App() {
   return (
     <div className="App">
       <Hero />
       <Programs />
+      <Reasons />
     </div>
   );
 }

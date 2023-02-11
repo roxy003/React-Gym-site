57b5beee0d0d93a2cb426edf50f42ea58da424c4
roxy003 <monalshah03@gmail.com>
2023-02-12 03:18:49 +0530

    making commits

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Programs from "./components/Programs/Programs";
 import Reasons from "./components/Reasons/Reasons";
 import Plans from "./components/Plans/Plans";
 import Testimonials from "./components/Testimonials/Testimonials";
+import Join from "./components/Join/Join";
 //Needed to import this file compulsory it seems so
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Reasons />
       <Plans />
       <Testimonials />
+      <Join />
     </div>
   );
 }

320be23fae2e19094dfa8af035ab6d5ac2ed8dca
roxy003 <monalshah03@gmail.com>
2023-02-11 20:01:08 +0530

    making commits

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Hero from "./components/Hero/Hero";
 import Programs from "./components/Programs/Programs";
 import Reasons from "./components/Reasons/Reasons";
+import Plans from "./components/Plans/Plans";
 //Needed to import this file compulsory it seems so
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <Programs />
       <Reasons />
+      <Plans />
     </div>
   );
 }
